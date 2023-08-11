fb04ec05946a7bf8f0751e7b1426a72a2867bc57
sahilbhat079 <sahilgani.sahilgani@gmail.com>
2023-08-11 15:42:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let hr = document.getElementById("hour");
+let min = document.getElementById("minute");
+let sec = document.getElementById("second");
+
+function displaytime(){
+let date = new Date();
+let hh =date.getHours();
+console.log(hh);
+let ss =date.getSeconds();
+let mm = date.getMinutes();
+ 
+let hrotation = 30*hh + mm/2;
+let mrotation = 6*mm;
+let srotation = 6*ss;
+hr.style.transform=`rotate(${hrotation}deg)`;
+min.style.transform=`rotate(${mrotation}deg)`;
+sec.style.transform=`rotate(${srotation}deg)`;
+
+
+
+}
+
+
+
+setInterval(displaytime,1000);
